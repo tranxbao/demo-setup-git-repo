7a1816a41de06d90af2e7f466a0d1c3a0d111cf7
Bao <tranxbao@gmail.com>
2022-10-10 18:41:53 +0300

    .env file modified

--- src/components/Weather/weatherService.js
+++ src/components/Weather/weatherService.js
@@ -3,7 +3,7 @@
 const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
 
 const getFormattedWeatherData = async(city, units='metric') =>{
-    const URL =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_MY_API_KEY}&units=${units}`;
+    const URL =`${process.env.REACT_APP_MY_API_URL}?q=${city}&appid=${process.env.REACT_APP_MY_API_KEY}&units=${units}`;
 
     const data = await fetch (URL)
     .then((res)=> res.json())
